9f888c19fddd04eed454d71b1ce5301861a5e8a5
wong030 <wongj@gmx.de>
2023-10-29 15:32:05 +0100

    feat: Maven, Spring Project added

---
+++ rateme/src/test/java/com/app/rateme/RatemeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.rateme;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RatemeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
