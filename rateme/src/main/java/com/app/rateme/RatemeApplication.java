c3c4e8614d850174842f541bce9ee034a49627b6
JONAS GOLDNIK <abat.goldnik@extaccount.com>
2023-10-30 19:34:05 +0100

    feat: add monitoring services

--- rateme/src/main/java/com/app/rateme/RatemeApplication.java
+++ rateme/src/main/java/com/app/rateme/RatemeApplication.java
@@ -2,12 +2,23 @@ package com.app.rateme;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 public class RatemeApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(RatemeApplication.class, args);
+	}
+
+	@RestController
+	class Hello {
+
+		@RequestMapping
+		String hello() {
+			return "Hello World";
+		}
 	}
 
 }
