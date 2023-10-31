05fafdd28508cc1bfe10b4bec1f4502c5ed87817
JONAS GOLDNIK <abat.goldnik@extaccount.com>
2023-10-31 13:06:37 +0100

    fix: typo in echo command

--- rateme/start.sh
+++ rateme/start.sh
@@ -5,5 +5,5 @@ echo "Downing running services"
 docker compose down
 
 
-echo: "Starting springboot and monitoring services parallel..."
+echo "Starting springboot and monitoring services parallel..."
 docker compose up & docker compose  -f ./monitoring/docker-compose.yml up
