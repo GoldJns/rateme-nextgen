a764d56d3bbb5128dda27986e19f63976182074f
JONAS GOLDNIK <abat.goldnik@extaccount.com>
2023-10-31 12:27:32 +0100

    feat: add loki labels to identify containers in grafana

--- rateme/src/main/java/com/app/rateme/RatemeApplication.java
+++ rateme/src/main/java/com/app/rateme/RatemeApplication.java
@@ -4,12 +4,8 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import io.micrometer.observation.Observation;
-import io.micrometer.observation.ObservationRegistry;
 
 @SpringBootApplication
 public class RatemeApplication {
@@ -19,9 +15,8 @@ public class RatemeApplication {
 	}
 
 	@RestController
-	@Service
+
 	class Hello {
-		private ObservationRegistry observationRegistry;
 
 		private Logger logger = LoggerFactory.getLogger(RatemeApplication.class);
 
@@ -30,14 +25,9 @@ public class RatemeApplication {
 			logger.debug("this is a debug log");
 			logger.info("moinsen");
 			logger.error("this is exception");
-			return Observation
-					.createNotStarted("helloService", observationRegistry)
-					.observe(() -> this.helloNoObserve());
+			return "Hello World!";
 		}
 
-		String helloNoObserve() {
-			return "Hello World NoObserve!";
-		}
 	}
 
 }
