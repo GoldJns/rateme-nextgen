288593bcb0d690859853c207953d112387880023
JONAS GOLDNIK <abat.goldnik@extaccount.com>
2023-12-09 17:06:27 +0100

    feat: add initial-tf-files

---
+++ terraform/variables.tf
@@ -0,0 +1,15 @@
+  locals {
+      labels = {
+          "data-project" = var.data-project
+      }
+  }
+
+  variable "project" {
+      type= string
+      description = "ID Google project"
+  }
+
+  variable "region" {
+      type= string
+      description = "Region Google project"
+  }
