93b16057a460399b93c6b2ff18198e6ddbea0b48
JONAS GOLDNIK <abat.goldnik@extaccount.com>
2023-12-16 11:31:43 +0100

    chore: test helm deployment

--- deployment/deployAll.sh
+++ deployment/deployAll.sh
@@ -2,12 +2,18 @@
 
 echo "Deploying services using Helm..."
 
-NAMESPACE="default"
+if [ -z "$1" ]; then
+  NAMESPACE="default"
+else
+  NAMESPACE="$1"
+fi
 
+
+echo "Using namespace $1"
 deploy_with_helm() {
     local release_name=$1
     local chart_path=$2
-
+    echo "Deploy $release_name"
     helm upgrade --install $release_name $chart_path --namespace $NAMESPACE
 }
 
