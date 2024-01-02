945238df69457507507d5d7ca140dad6ca78412b
JONAS GOLDNIK <abat.goldnik@extaccount.com>
2024-01-02 21:41:32 +0100

    fix: terraform scripts

--- terraform/common.tf
+++ terraform/common.tf
@@ -4,17 +4,6 @@ resource "google_service_account" "gke_prod_service_account" {
   project      = "rateme-nextgen"
 }
 
-resource "kubernetes_service_account" "prod_service_account" {
-  metadata {
-    name      = "prod-service-account"
-    namespace = "prod"
-
-    annotations = {
-      "iam.gke.io/gcp-service-account" = "${google_service_account.gke_prod_service_account.email}"
-    }
-  }
-}
-
 
 resource "google_project_iam_binding" "secretmanager_access_binding" {
   project = "rateme-nextgen"
--- terraform/secretmanager.tf
+++ terraform/secretmanager.tf
@@ -1,20 +1,20 @@
 
-resource "google_secret_manager_secret" "my_secret" {
-  project   = var.project
+resource "google_secret_manager_secret" "db_secret" {
+  project = var.project
   replication {
     user_managed {
       replicas {
         location = "us-central1"
       }
     }
-    
+
   }
-  secret_id = "my_secret"
-  labels    = { label = "rateme-nextgen-secret"}
+  secret_id = "db_secret"
+  labels    = { label = "rateme-nextgen-secret" }
 }
 
-resource "google_secret_manager_secret_version" "my_secret_version" {
-  secret      = google_secret_manager_secret.my_secret.id
+resource "google_secret_manager_secret_version" "db_secret_version" {
+  secret      = google_secret_manager_secret.db_secret.id
   secret_data = var.mysql_password
 }
 
