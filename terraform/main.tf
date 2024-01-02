950a3d3910af64e600f12b5bacffdd0da22b07c7
JONAS GOLDNIK <abat.goldnik@extaccount.com>
2024-01-02 21:45:20 +0100

    fix: k8 ressource type

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,39 +1,23 @@
-    terraform {
-      backend "gcs" {
-        bucket = "terraform-state-rateme-nextgen"
-        prefix = "prod"
-      }
-    }
+terraform {
+  backend "gcs" {
+    bucket = "terraform-state-rateme-nextgen"
+    prefix = "prod"
+  }
+}
 
-    provider "google" {
-      project = var.project
-      region  = var.region
-    }
+provider "google" {
+  project = var.project
+  region  = var.region
+}
 
 resource "google_container_cluster" "gke_cluster" {
   name                     = var.cluster-name
   location                 = var.region
   remove_default_node_pool = true
   initial_node_count       = 1
-  deletion_protection = false
+  deletion_protection      = false
 }
 
-
-
-provider "kubernetes" {
-  host = "https://${google_container_cluster.primary.endpoint}"
-  username = "${google_container_cluster.primary.master_auth.0.username}"
-  password = "${google_container_cluster.primary.master_auth.0.password}"
-  client_certificate = "${base64decode(google_container_cluster.primary.master_auth.0.client_certificate)}"
-  client_key = "${base64decode(google_container_cluster.primary.master_auth.0.client_key)}"
-  cluster_ca_certificate = "${base64decode(google_container_cluster.primary.master_auth.0.cluster_ca_certificate)}"
-}
-
-resource "kubernetes_namespace" "n" {
-  metadata {
-    name = "dev"
-  }
-}
 
 
 resource "google_container_node_pool" "gke_nodes" {
