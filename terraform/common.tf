b7c4e6aca5c6fedcf1575e91bc21b285866d2022
JONAS GOLDNIK <abat.goldnik@extaccount.com>
2024-01-03 12:08:58 +0100

    fix: iam bindings

--- terraform/common.tf
+++ terraform/common.tf
@@ -15,7 +15,7 @@ resource "google_project_iam_binding" "secretmanager_access_binding_prod" {
   role    = "roles/secretmanager.secretAccessor"
 
   members = [
-    "serviceAccount:${google_service_account.gke_prod_service_account.email}"
+    "serviceAccount:rateme-nextgen.svc.id.goog[prod/prod-service-account]"
   ]
 }
 
@@ -24,6 +24,24 @@ resource "google_project_iam_binding" "secretmanager_access_binding_dev" {
   role    = "roles/secretmanager.secretAccessor"
 
   members = [
-    "serviceAccount:${google_service_account.gke_dev_service_account.email}"
+    "serviceAccount:rateme-nextgen.svc.id.goog[dev/dev-service-account]"
+  ]
+}
+
+
+resource "google_project_iam_binding" "secretmanager_workload_binding_prod" {
+  project = "rateme-nextgen"
+  role    = "roles/iam.workloadIdentityUser"
+
+  members = [
+    "serviceAccount:rateme-nextgen.svc.id.goog[prod/prod-service-account]"
+  ]
+}
+
+resource "google_project_iam_binding" "secretmanager_workload_binding_dev" {
+  project = "rateme-nextgen"
+  role    = "roles/iam.workloadIdentityUser"
+  members = [
+    "serviceAccount:rateme-nextgen.svc.id.goog[dev/dev-service-account]"
   ]
 }
