ba996fe14314dae3e3eed4e215af48c7ac48a6da
JONAS GOLDNIK <abat.goldnik@extaccount.com>
2024-01-03 11:48:28 +0100

    fix: gke service accounts

--- terraform/common.tf
+++ terraform/common.tf
@@ -4,8 +4,13 @@ resource "google_service_account" "gke_prod_service_account" {
   project      = "rateme-nextgen"
 }
 
+resource "google_service_account" "gke_dev_service_account" {
+  account_id   = "gke-dev-service-account"
+  display_name = "GKE Dev Service Account"
+  project      = "rateme-nextgen"
+}
 
-resource "google_project_iam_binding" "secretmanager_access_binding" {
+resource "google_project_iam_binding" "secretmanager_access_binding_prod" {
   project = "rateme-nextgen"
   role    = "roles/secretmanager.secretAccessor"
 
@@ -14,5 +19,11 @@ resource "google_project_iam_binding" "secretmanager_access_binding" {
   ]
 }
 
+resource "google_project_iam_binding" "secretmanager_access_binding_dev" {
+  project = "rateme-nextgen"
+  role    = "roles/secretmanager.secretAccessor"
 
-
+  members = [
+    "serviceAccount:${google_service_account.gke_dev_service_account.email}"
+  ]
+}
