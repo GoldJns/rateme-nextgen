9eaf5aba2ea88ae392a30825e8fb571693bc9e9d
JONAS GOLDNIK <abat.goldnik@extaccount.com>
2024-01-09 10:37:36 +0100

    refactor: secrets

--- terraform/secretmanager.tf
+++ terraform/secretmanager.tf
@@ -1,5 +1,5 @@
 
-resource "google_secret_manager_secret" "db_secret" {
+resource "google_secret_manager_secret" "gke_mysql_password" {
   project = var.project
   replication {
     user_managed {
@@ -9,12 +9,31 @@ resource "google_secret_manager_secret" "db_secret" {
     }
 
   }
-  secret_id = "db_secret"
+  secret_id = "gke_mysql_password"
   labels    = { label = "rateme-nextgen-secret" }
 }
 
+resource "google_secret_manager_secret" "gke_mysql_root_password" {
+  project = var.project
+  replication {
+    user_managed {
+      replicas {
+        location = "us-central1"
+      }
+    }
+
+  }
+  secret_id = "gke_mysql_root_password"
+  labels    = { label = "rateme-nextgen-secret" }
+}
+
+
 resource "google_secret_manager_secret_version" "db_secret_version" {
-  secret      = google_secret_manager_secret.db_secret.id
+  secret      = google_secret_manager_secret.gke_mysql_password.id
   secret_data = var.mysql_password
 }
 
+resource "google_secret_manager_secret_version" "db_secret_root_version" {
+  secret      = google_secret_manager_secret.gke_mysql_root_password.id
+  secret_data = var.mysql_password
+}
