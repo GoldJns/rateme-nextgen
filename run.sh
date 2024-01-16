96b2b32989fd40ac20b7e8a180208c93c2dc613d
JONAS GOLDNIK <abat.goldnik@extaccount.com>
2024-01-16 17:58:27 +0100

    chore: remove monitoring project docker compose

--- run.sh
+++ run.sh
@@ -5,6 +5,6 @@ docker compose down
 
 
 echo "Starting springboot and monitoring services parallel..."
-docker compose up & docker compose  -f ./monitoring/compose/docker-compose-monitoring.yml up
+docker compose up --build
 
 $SHELL
