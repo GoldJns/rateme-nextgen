9a6a4549845d83001bc06ac4973b25b2ff045320
JONAS GOLDNIK <abat.goldnik@extaccount.com>
2024-01-31 15:49:02 +0100

    fix: remove value

--- src/main/java/com/app/rateme/security/JwtUtil.java
+++ src/main/java/com/app/rateme/security/JwtUtil.java
@@ -19,7 +19,7 @@ import io.jsonwebtoken.security.Keys;
 public class JwtUtil {
 
     @Value("${JWT_SECRET}")
-    public static final String SECRET = "5367566B59703373367639792F423F4528482B4D6251655468576D5A71347437";
+    public static final String SECRET;
 
     public String extractUsername(String token) {
         return extractClaim(token, Claims::getSubject);
